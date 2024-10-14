49481130401e90eb74aeacfea037327a454ab253
Anselmo Jorge <acambinza@cetim.ms>
2024-10-14 16:41:19 +0100

    fix: nginx config

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN mkdir -p /home/app/node_modules && chown -R node:node /home/app
 WORKDIR /home/app
 USER node
 COPY --chown=node:node . .
-RUN npm install -f
+RUN npm install
 RUN npm run build
 
 # Stage Two
--- install.sh
+++ install.sh
@@ -1,69 +1,16 @@
-#! /bin/bash
+#!/bin/sh
 
-echo "Welcome to Install Script"
-echo "Choose what you want to install"
+echo "Installing Noah landingpage in production environment"
 
+echo "Give the proper permissions in cert files"
+chmod 755 ./ssl/
+chmod 604 ./ssl/*
+echo "End of the process of giving proper permissions"
 
-function installDev()
-{
-    echo "Install Dev Started"
-    sleep 1
+echo "Starting instalation..."
 
-    yarn 
+docker-compose down --rmi all
+docker-compose build --no-cache
+docker-compose up -d
 
-    sleep 3
-    yarn dev --host --port 3000
-
-    #docker-compose -f docker-compose_dev.yml down
-    #docker-compose -f docker-compose_dev.yml build --no-cache
-    #docker-compose -f docker-compose_dev.yml up
-
-    #echo "Install Dev Finished"
-
-    exit 0
-}
-
-function installProd()
-{
-    echo "Install Prod Started"
-    sleep 1
-
-    docker-compose down --rmi all
-    docker-compose build --no-cache
-    docker-compose up -d
-
-    sleep 1
-    echo "Install Prod Finished"
-
-    echo "Containers up"
-    docker-compose ps
-    exit 0
-}
-
-
-
-select op in DEV PROD EXIT
-do 
-    case $op in 
-        DEV)
-            echo "DEV"
-            installDev
-        ;;
-
-        PROD)
-            echo "PROD"
-            installProd
-        ;;
-
-        EXIT)
-            echo "Terminated successfully"
-            exit 0
-        ;;
-
-        *)
-            echo "Error, unknown command";;
-    
-    esac
-done
-
-
+echo "Ending instalation"
